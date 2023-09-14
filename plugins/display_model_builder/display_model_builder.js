5055945901bcdb1b06d7075bcd7f7a804391dacd
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2023-09-13 19:14:38 -0500

    lot o work

--- plugins/display_model_builder/display_model_builder.js
+++ plugins/display_model_builder/display_model_builder.js
@@ -26,7 +26,7 @@
         }
     }
 
-    let format, action, dialog, mode, panel, styles, preview, debug, stats
+    let format, action, panel, property
 
     Plugin.register(id, {
         title: name,
@@ -39,196 +39,6 @@
         variant: "both",
         creation_data: "2023-09-02",
         async onload() {
-
-            styles = Blockbench.addCSS(`
-                body:not(.is_mobile) #work_screen:has(#panel_display_model_render_panel:not(.hidden)) {
-                    grid-template-columns: 0px auto 0 !important;
-                }
-                #panel_display_model_render_panel > .panel_handle, #work_screen:has(#panel_display_model_render_panel:not(.hidden)) > .resizer.vertical {
-                    display: none !important;
-                }
-                #work_screen:has(#panel_display_model_render_panel:not(.hidden)) > #center {
-                    margin-bottom: calc(4px - var(--toolbar-height));
-                }
-                #panel_display_model_render_panel {
-                    width: 0px !important; 
-                    height: 0px !important;
-                }
-                #display-model-render-controls-container {
-                    position: absolute;
-                    left: 0;
-                    right: 0;
-                    bottom: 50px;
-                    display: flex;
-                    justify-content: center;
-                }
-                #display-model-render-controls {
-                    background-color: var(--color-ui);
-                    z-index: 2;
-                    padding: 10px;
-                    box-shadow: 0 5px 10px #0006;
-                    display: flex;
-                    gap: 10px;
-                    align-items: center;
-                    flex-direction: column;
-                }
-                #display-model-code-container {
-                    overflow: auto;
-                    flex: 1;
-                    max-height: calc(100% - 40px);
-                    min-height: 3.5em;
-                    display: flex;
-                }
-                #display-model-code {
-                    cursor: text;
-                    flex: 1;
-                }
-                .display-model-render-controls-row {
-                    display: flex;
-                    gap: 20px;
-                    align-items: center;
-                }
-                #display-model-render-button {
-                    background-color: var(--color-close);
-                    color: var(--color-light);
-                    padding: 10px 30px 1px;
-                    border-radius: 8px;
-                    cursor: pointer;
-                    transition: filter .15s;
-                }
-                #display-model-render-button:hover {
-                    filter: brightness(1.25) hue-rotate(5deg);
-                }
-                #display-model-render-button.disabled {
-                    background-color: var(--color-button);
-                    cursor: not-allowed;
-                }
-                display-model-render-button.disabled:hover {
-                    filter: initial;
-                }
-                .display-model-button {
-                    cursor: pointer;
-                    border-radius: 4px;
-                }
-                .display-model-button.selected {
-                    background-color: var(--color-accent);
-                    color: var(--color-accent_text);
-                }
-                .display-model-button.selected > svg {
-                    fill: var(--color-accent_text);
-                }
-                #resolutions {
-                    display: flex;
-                    background-color: var(--color-button);
-                    padding: 4px;
-                }
-                .resolution {
-                    width: 32px;
-                    height: 32px;
-                }
-                .resolution > i {
-                    font-size: 32px;
-                }
-                .resolution > svg {
-                    fill: var(--color-text);
-                }
-                .display-model-list {
-                    display: flex;
-                    max-height: 384px;
-                    flex-wrap: wrap;
-                    gap: 10px;
-                    overflow-x: hidden;
-                }
-                .display-model-list.small {
-                    max-height: 198px;
-                }
-                .display-model-item {
-                    display: flex;
-                    flex-direction: column;
-                    gap: 5px;
-                    padding: 10px 10px 5px;
-                    background-color: var(--color-back);
-                    align-items: center;
-                    cursor: pointer;
-                    flex: 1;
-                    position: relative;
-                }
-                .display-model-item:hover {
-                    background-color: var(--color-button);
-                }
-                .display-model-item > img {
-                    max-width: 100px;
-                    height: 50px;
-                    display: flex;
-                    object-fit: contain;
-                }
-                .display-model-item > div {
-                    text-align: center;
-                    flex: 1;
-                    display: flex;
-                    align-items: center;
-                }
-                .display-model-item.selected {
-                    outline: 2px solid var(--color-accent);
-                    background-color: var(--color-button);
-                    outline-offset: -2px;
-                }
-                .display-model-item-has-variants {
-                    display: flex;
-                    position: absolute;
-                    bottom: 4px;
-                    right: 4px;
-                    z-index: 1;
-                }
-                .display-model-links {
-                    display: flex;
-                    justify-content: space-around;
-                    margin: 20px 40px 0;
-                }
-                .display-model-links > a {
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    gap: 5px;
-                    padding: 5px;
-                    text-decoration: none;
-                    flex-grow: 1;
-                    flex-basis: 0;
-                    color: var(--color-subtle_text);
-                    text-align: center;
-                }
-                .display-model-links > a:hover {
-                    background-color: var(--color-accent);
-                    color: var(--color-light);
-                }
-                .display-model-links > a > i {
-                    font-size: 32px;
-                    width: 100%;
-                    max-width: initial;
-                    height: 32px;
-                    text-align: center;
-                }
-                .display-model-links > a:hover > i {
-                    color: var(--color-light) !important;
-                }
-                .display-model-links > a > p {
-                    flex: 1;
-                    display: flex;
-                    align-items: center;
-                }
-                .spacer, #display_model_generator .sp-preview, #display_model_generator .form_inline_select > li {
-                    flex: 1;
-                }
-                .display-model-item-buttons > i:hover, #display-model-preview-container > i:hover, .display-model-button:hover, #display-model-custom-texture > i:hover, .text-input-row > i:hover, .display-model-button:hover > svg {
-                    color: var(--color-light);
-                }
-                .display-model-preset > *, .display-model-item *, .text-input-row > i:hover, #display-model-render-button-row *, .display-model-links * {
-                    cursor: pointer;
-                }
-                #format_page_display_model {
-                    padding-bottom: 0;
-                }
-            `)
 
             format = new ModelFormat({
                 id: "display_model",
@@ -303,8 +113,8 @@
                         VuePrismEditor
                     },
                     data: {
-                        text: "// All of the Code will go here\n// Just Copy & Paste it into your Minecraft Plugin!",
-                        readonly: true
+                        text: `// There is nothing to display!
+// Try adding some cubes to get started!`
                     },
                     methods: {
                         update() {
@@ -313,20 +123,41 @@
                     },
                     template: `
                         <div>
-                            <div id="display_model_code_container">
-                                <vue-prism-editor id="display_model_code" v-model="text" readonly=true language="java" line-numbers />
-                            </div>
+                            <vue-prism-editor v-model="text" language="json" readonly=true line-numbers />
                         </div>
                     `
                 }
             })
 
-            Blockbench.on("finished_edit", () => {
+            property = new Property(Cube, 'string', 'material', {
+                default: "stone",
+                exposed: true
+            });
+
+            action = new Action("change_material", {
+                name: "Change Material",
+                description: "Change the material of the cube, this is used for the generated block display!",
+                icon,
+                condition: {
+                    formats: [format.id]
+                }
+                
+            })
+
+            Cube.prototype.menu.addAction(action, 8)
+
+            Blockbench.on("finished_edit", updatePanel)
+
+            function updatePanel() {
                 if (Project.format?.id == "display_model") {
                     let code = generateCode();
                     panel.vue.text = code;
                 }
-            })
+            }
+
+            function nameToId(name) {
+                return name.toLowerCase().replaceAll(" ", "_");
+            }
 
             function generateCode() {
                 let code = `new ModelBuilder()`;
@@ -336,20 +167,42 @@
 // Try adding some cubes to get started!`
                 }
 
+                let ids = new Set();
+                let duplicate = false;
                 elements.forEach(element => {
-                    let size = element.getMesh().scale;
-                    let position = element.getMesh().position;
+                    let id = nameToId(element.name);
+                    if (ids.has(id)) {
+                        duplicate = true;
+                    }
+                    ids.add(id);
+                })
+
+                if (duplicate) {
+                    return `// There are duplicate id's!
+// Change the names to remove duplicates!`
+                }
+
+                elements.forEach(element => {
+                    let from = element.from;
+                    let to = element.to;
                     let rotation = element.getMesh().rotation;
-                    let id = element.name.toLowerCase().replace(" ", "_");
+                    let scale = [Math.abs(from[0] - to[0]), Math.abs(from[1] - to[1]), Math.abs(from[2] - to[2])];
+                    let position = [(from[0] + to[0]) / 2, (from[1] + to[1]) / 2, (from[2] + to[2]) / 2]
+                    let material = element.material.toUpperCase();
+                    let id = nameToId(element.name);
+
                     code = code + `
     .add(\"${id}\", new ModelCuboid()
-        .size(${size.x}, ${size.y}, ${size.z})
-        .position(${position.x}, ${position.y}, ${position.z})
+        .material(Material.${material})
+        .size(${scale[0]}, ${scale[1]}, ${scale[2]})
+        .position(${position[0]}, ${position[1]}, ${position[2]})
         .rotation(${rotation.x}, ${rotation.y}, ${rotation.z}))`
                 })
 
                 return code
             }
+
+            updatePanel()
         }
     });
 })()
